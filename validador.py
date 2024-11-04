1441ed8c28dbe5f6ca4457b8d3cf5349cdceed86
Leocadio-dev <lucasleocadio05@gmail.com>
2024-11-04 12:17:47 -0300

    half of the validation completed in the 'calcularDoisUltimosDigitos' funcion

--- validador.py
+++ validador.py
@@ -1,49 +1,65 @@
 def main():
     
-    # CNPJ será string para fazer o split
-    cnpj = input("Digite o CNPJ que será validado (somente os números)")
+    # CPF será string para fazer o split
+    cpf = input("Digite o CPF que será validado (somente os números)")
     
     
-    # Verificando se o tamanho do cnpj é válido
-    while len(cnpj) != 11:
-        print("Cnpj inválido porque é menor que o tamanho padrão")
-        cnpj = input("Digite o CNPJ que será validado (somente os números)")
+    # Verificando se o tamanho do cpf é válido
+    while len(cpf) != 11:
+        print("Cpf inválido porque é menor que o tamanho padrão")
+        cpf = input("Digite o CPF que será validado (somente os números)")
         
 
     # Armazenando o valor booleano do validador
     
-    resultadoValidacao = validador(cnpj)
+    resultadoValidacao = validador(cpf)
     
     if resultadoValidacao == True:
-        print(f"O cnpj: {cnpj} é válido")
+        print(f"O cpf: {cpf} é válido")
     else:
-        print(f"O cnpj: {cnpj} não é válido")
+        print(f"O cpf: {cpf} não é válido")
 
 
-def validador(cnpj: str):
+def validador(cpf: str):
 
-    
-    listNumerosCnpj = [] # Criando lista para gerar um split do cnpj
+    listNumerosCpf = []
+    listNumerosCpfModificado = [] # Criando lista para gerar um split do cpf
     listaAux = [] # Lista com os números multiplicados
 
-    for char in len(cnpj) :
-        listNumerosCnpj.append(int(char)) # Adicionando o caractere na lista
-        if len(listNumerosCnpj) == 9: 
-            break # Precisa parar o laço para verificar 
-    listNumerosCnpj.reverse() # Revertendo a lista 
+    # Laço pra dividir todo o cpf
+    for char in cpf:
+        listNumerosCpf.append(int(char))
+
+    # Laço pra reverter a lista
+    for char in cpf :
+        listNumerosCpfModificado.append(int(char)) # Adicionando o caractere na lista
+        if len(listNumerosCpfModificado) == 9: 
+            break # Precisa parar o laço para verificar os dois últimos dígitos
+    listNumerosCpfModificado.reverse() # Revertendo a lista 
 
     multiplicadorAuxiliar = 2 # Começa multiplicando por 2
     somaNumeros = 0
-    for i in range(len(listNumerosCnpj)):
-        listaAux.append(listNumerosCnpj[i] * multiplicadorAuxiliar) # Adicionando os números já multiplicados
+    for i in range(len(listNumerosCpfModificado)):
+        listaAux.append(listNumerosCpfModificado[i] * multiplicadorAuxiliar) # Adicionando os números já multiplicados
         multiplicadorAuxiliar += 1
         somaNumeros += listaAux[i]
 
-    calcularDoisUltimosDigitos(listNumerosCnpj, listaAux, somaNumeros)
-    print(listaAux)
+    calcularDoisUltimosDigitos(listNumerosCpf, listaAux, somaNumeros) # Lista com cpf splitado, lista com os valores somados, soma total
+
     return 1
 
-def calcularDoisUltimosDigitos(listlistNumerosCnpj: list, listaAux: list, somaNumeros: int):
+def calcularDoisUltimosDigitos(listNumerosCpf: str, listaAux: list, somaNumeros: int):
+
+    validadoresUltimosDigitos = [] # Lista que vai guardar o resultado da divisão e o resto
+    ultimosDigitos = [] # Últimos dígitos do cpf (na ordem normal)
+
+    validadoresUltimosDigitos[0] = somaNumeros / 11   # Valor da divisão (divisão por 11 porque é o tamanho padrão do cpf)
+    validadoresUltimosDigitos[1] = somaNumeros % 11   # Resto da divisão 
+    
+    if validadoresUltimosDigitos[1] < 2:
+        ultimosDigitos[0] = 0
+    else:
+        ultimosDigitos[0] = 11 - validadoresUltimosDigitos[1]
 
     return 
        
